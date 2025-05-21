3d29b975a3d952e146cbbe88a2abdf5c7ed5deae
svetlanapostel <svdzalaty33@gmail.com>
2025-05-21 14:08:24 +0200

    Add JWT token endpoints: obtain, refresh, verify

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from apps.users.views import RegisterAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
-
